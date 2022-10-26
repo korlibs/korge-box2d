a6b6dc45e703cedbbdd7f8c1072eb0ae67322bd3
soywiz <soywiz@gmail.com>
2022-10-26 13:46:17 +0200

    Initial import

---
+++ korge-box2d/src/jsMain/kotlin/org/jbox2d/internal/System_nanoTime.kt
@@ -0,0 +1,6 @@
+package org.jbox2d.internal
+
+import kotlin.js.Date
+
+//actual fun System_nanoTime(): Long = window.performance.now().toLong() // @TODO: node.js: https://nodejs.org/api/process.html#process_process_hrtime_time
+actual fun System_nanoTime(): Long = (Date.now().toLong() * 1000000L)
