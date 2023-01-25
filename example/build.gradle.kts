83ed078cb6285317a072ec9822275f54984355de
soywiz <soywiz@gmail.com>
2023-01-25 20:19:38 +0100

    Update box2d

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -2,11 +2,11 @@ import com.soywiz.korge.gradle.*
 
 plugins {
         //alias(libs.plugins.korge)
-    id("com.soywiz.korge") version "3.3.0"
+    id("com.soywiz.korge") version "4.0.0-alpha-2"
 }
 
 korge {
-    id = "com.sample.demodragonbones"
+    id = "org.korge.samples.box2d"
 
 // To enable all targets at once
 
@@ -19,12 +19,11 @@ korge {
 
     targetJvm()
     targetJs()
-    //targetDesktop()
-    //targetDesktopCross()
-    //targetIos()
-    //targetAndroidIndirect() // targetAndroidDirect()
+    targetDesktop()
+    targetDesktopCross()
+    targetIos()
+    targetAndroidDirect()
     serializationJson()
-    //targetAndroidDirect()
 }
 
 dependencies {
