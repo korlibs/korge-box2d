eb3ed14228acbb6e8a91be0f4ae3db7d3d5fff65
soywiz <soywiz@gmail.com>
2023-06-17 18:07:30 +0200

    Bumps versions of korge and kproject

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import korlibs.korge.gradle.*
 
 plugins {
     //alias(libs.plugins.korge)
-    id("com.soywiz.korge") version "4.0.0-rc4"
+    id("com.soywiz.korge") version "4.0.6"
 }
 
 korge {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 pluginManagement { repositories {  mavenLocal(); mavenCentral(); google(); gradlePluginPortal()  }  }9
 plugins {
     //id("com.soywiz.kproject.settings") version "0.0.1-SNAPSHOT"
-    id("com.soywiz.kproject.settings") version "0.2.9"
+    id("com.soywiz.kproject.settings") version "0.3.1"
 }
 
 rootProject.name = "${rootDir.name}-example"
